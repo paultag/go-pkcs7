614ce5b33b5d9c46860ff0c6ed9be6d205501fd1
Paul Tagliamomnte <tag@pault.ag>
2016-12-11 14:06:46 -0500

    add better certs

--- pkcs7_test.go
+++ pkcs7_test.go
@@ -16,9 +16,12 @@ import (
 	"pault.ag/go/pkcs7"
 )
 
-var cert *x509.Certificate
-var rsaPrivateKey *rsa.PrivateKey
-var someOtherRsaPrivateKey *rsa.PrivateKey
+var aliceCert *x509.Certificate
+var bobCert *x509.Certificate
+
+var aliceRsaPrivateKey *rsa.PrivateKey
+var bobRsaPrivateKey *rsa.PrivateKey
+var malloryRsaPrivateKey *rsa.PrivateKey
 
 func assert(t *testing.T, condition bool, what string) {
 	if !condition {
@@ -39,7 +42,7 @@ func ok(t *testing.T, err error) {
 func TestEncryption(t *testing.T) {
 	contentInfo, err := pkcs7.Encrypt(
 		rand.Reader,
-		[]x509.Certificate{*cert},
+		[]x509.Certificate{*aliceCert},
 		SecretData,
 	)
 	ok(t, err)
@@ -51,7 +54,7 @@ func TestEncryption(t *testing.T) {
 func TestEncryptionRoundTrip(t *testing.T) {
 	contentInfo, err := pkcs7.Encrypt(
 		rand.Reader,
-		[]x509.Certificate{*cert},
+		[]x509.Certificate{*aliceCert},
 		SecretData,
 	)
 	ok(t, err)
@@ -65,7 +68,7 @@ func TestEncryptionRoundTrip(t *testing.T) {
 	envelopedData, err := contentInfo.EnvelopedData()
 	ok(t, err)
 
-	secretData, err := envelopedData.Decrypt(*cert, rsaPrivateKey, rand.Reader, nil)
+	secretData, err := envelopedData.Decrypt(*aliceCert, aliceRsaPrivateKey, rand.Reader, nil)
 	ok(t, err)
 
 	assert(t, bytes.Compare(secretData, SecretData) == 0, "decrypted data doesn't match test corpus")
@@ -74,14 +77,14 @@ func TestEncryptionRoundTrip(t *testing.T) {
 func TestSign(t *testing.T) {
 	dataContentInfo, err := pkcs7.Data(SecretData)
 	ok(t, err)
-	_, err = pkcs7.Sign(rand.Reader, *dataContentInfo, *cert, rsaPrivateKey, crypto.SHA256)
+	_, err = pkcs7.Sign(rand.Reader, *dataContentInfo, *aliceCert, aliceRsaPrivateKey, crypto.SHA256)
 	ok(t, err)
 }
 
 func TestSignAndVerify(t *testing.T) {
 	dataContentInfo, err := pkcs7.Data(SecretData)
 	ok(t, err)
-	signedContentInfo, err := pkcs7.Sign(rand.Reader, *dataContentInfo, *cert, rsaPrivateKey, crypto.SHA256)
+	signedContentInfo, err := pkcs7.Sign(rand.Reader, *dataContentInfo, *aliceCert, aliceRsaPrivateKey, crypto.SHA256)
 	ok(t, err)
 
 	signedContentInfoBytes, err := asn1.Marshal(*signedContentInfo)
@@ -93,13 +96,13 @@ func TestSignAndVerify(t *testing.T) {
 	underlyingContentInfo, err := contentInfo.SignedData()
 	ok(t, err)
 
-	ok(t, underlyingContentInfo.Verify(*cert))
+	ok(t, underlyingContentInfo.Verify(*aliceCert))
 }
 
 func TestSignAndFailsToVerifyBadHash(t *testing.T) {
 	dataContentInfo, err := pkcs7.Data(SecretData)
 	ok(t, err)
-	signedContentInfo, err := pkcs7.Sign(rand.Reader, *dataContentInfo, *cert, rsaPrivateKey, crypto.SHA256)
+	signedContentInfo, err := pkcs7.Sign(rand.Reader, *dataContentInfo, *aliceCert, aliceRsaPrivateKey, crypto.SHA256)
 	ok(t, err)
 
 	signedContentInfo.Content.Bytes[100] = 'h'
@@ -113,13 +116,13 @@ func TestSignAndFailsToVerifyBadHash(t *testing.T) {
 	underlyingContentInfo, err := contentInfo.SignedData()
 	ok(t, err)
 
-	nokay(t, underlyingContentInfo.Verify(*cert), "Hash validation passed")
+	nokay(t, underlyingContentInfo.Verify(*aliceCert), "Hash validation passed")
 }
 
 func TestSignAndFailsToVerifySignature(t *testing.T) {
 	dataContentInfo, err := pkcs7.Data(SecretData)
 	ok(t, err)
-	signedContentInfo, err := pkcs7.Sign(rand.Reader, *dataContentInfo, *cert, someOtherRsaPrivateKey, crypto.SHA256)
+	signedContentInfo, err := pkcs7.Sign(rand.Reader, *dataContentInfo, *aliceCert, malloryRsaPrivateKey, crypto.SHA256)
 	ok(t, err)
 
 	signedContentInfoBytes, err := asn1.Marshal(*signedContentInfo)
@@ -131,30 +134,44 @@ func TestSignAndFailsToVerifySignature(t *testing.T) {
 	underlyingContentInfo, err := contentInfo.SignedData()
 	ok(t, err)
 
-	nokay(t, underlyingContentInfo.Verify(*cert), "Bogus signature was fine passed")
+	nokay(t, underlyingContentInfo.Verify(*aliceCert), "Bogus signature was fine passed")
 }
 
 func TestMain(m *testing.M) {
-	block, _ := pem.Decode([]byte(TestCert))
-	testCert, err := x509.ParseCertificate(block.Bytes)
+	block, _ := pem.Decode(AliceCert)
+	cert, err := x509.ParseCertificate(block.Bytes)
 	if err != nil {
 		panic(err)
 	}
-	cert = testCert
+	aliceCert = cert
 
-	block, _ = pem.Decode([]byte(TestKey))
+	block, _ = pem.Decode(BobsCert)
+	cert, err = x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	bobCert = cert
+
+	block, _ = pem.Decode(AliceKey)
 	rsaKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
 	if err != nil {
 		panic(err)
 	}
-	rsaPrivateKey = rsaKey
+	aliceRsaPrivateKey = rsaKey
 
-	block, _ = pem.Decode([]byte(SomeOtherKey))
+	block, _ = pem.Decode(BobsKey)
 	rsaKey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
 	if err != nil {
 		panic(err)
 	}
-	someOtherRsaPrivateKey = rsaKey
+	bobRsaPrivateKey = rsaKey
+
+	block, _ = pem.Decode(MalloryKey)
+	rsaKey, err = x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		panic(err)
+	}
+	malloryRsaPrivateKey = rsaKey
 
 	flag.Parse()
 	ret := m.Run()
@@ -162,7 +179,7 @@ func TestMain(m *testing.M) {
 }
 
 var (
-	TestCert string = `-----BEGIN CERTIFICATE-----
+	AliceCert []byte = []byte(`-----BEGIN CERTIFICATE-----
 MIIEXTCCAsWgAwIBAgIRAJB4MY12+llkm0LMilO286kwDQYJKoZIhvcNAQELBQAw
 WTELMAkGA1UEBhMCVVMxKjAoBgNVBAoTIVN0cmV4Q29ycCBTeW5lcm5pc3RzIElu
 Y29ycG9yYXRlZDEeMBwGA1UEAxMVS2V2aW5zLU1CUC5zdHJleC5jb3JwMB4XDTE2
@@ -188,8 +205,8 @@ j0cJQITCN+vAa4ydnZ6+86w2kt3R8xpyJY9y6bVkH55ft9kQlB4od6r+Q5Mr6eii
 lgmL9D1o2wVfDwWMU4PtNqLV9cYw+lelWxw02QLq+jXuF5ABUbbWhNHJMRSwFKLN
 Q+dboFBMPggDWrPrgWSk8bw=
 -----END CERTIFICATE-----
-`
-	TestKey string = `-----BEGIN RSA PRIVATE KEY-----
+`)
+	AliceKey []byte = []byte(`-----BEGIN RSA PRIVATE KEY-----
 MIIG5QIBAAKCAYEA11HE0IdpPCSlWHTIr0nXrUGUxcq5IMeTc6VIM0wigGjW0MFO
 6gzNnjVoiBITupsUAv9QjalTPpYI6tC+jX9sAbDtLrI/qU95YCinOsFaRfQNwGTX
 Gxumsepl6Bl/fTryMWaAZ/Bo8prJXT3H3XHpoXsmJTuZghq24zhXgxVEfz4tDmiJ
@@ -228,8 +245,8 @@ DXaeFmhgwUbcY/dmrFMP6w3JNLO/gBtGxqPThlyE9C1C7XiJUAs88i0CqUL+bpb9
 LxRjbauUde0pzZx8VUmbZ2AA9halL7cuSxBTwuTlVgfcZatlgh7TbbiXOSGsq6gi
 ZBCLq8bfn7ZZWn+AnVYijkrWUyjYFpz/hcpJ4VzMp7vEDcyxnh0fGOs=
 -----END RSA PRIVATE KEY-----
-`
-	SomeOtherKey string = `-----BEGIN RSA PRIVATE KEY-----
+`)
+	MalloryKey []byte = []byte(`-----BEGIN RSA PRIVATE KEY-----
 MIIEpAIBAAKCAQEA3j+sMete0f1BzXRRuYAAy4i3SdKITVhe9A9g0MG4GH4H96Oy
 Q8mEJYPqpSQ2QZlVv9ObVr1bIFMqBaWYEEpOtECBrfPN/nWxocCAl0hPI7hvGRV0
 TIMZ3uBITldJvgwwYJi2QcXADK5zsOGneroabGGik18VBEz6wyKDS9w8tVFRsD1A
@@ -256,7 +273,77 @@ eiow4e6YVu67vMW/ECp6WHzv9OSgJgZqsTMcq476ppfrSQHLiCF8qLDNrFdxlUzZ
 RiYfqbigxDM/TsoNEqkWj8lrgE+8tjp/q39iQL41vcNbt7nqCYc8ReQ1tKrC3j3L
 JhONIdLcifl7DeJMLPm9eSQeeTL7Tmqt/0m2kcJW6TrxcLls+RqtiQ==
 -----END RSA PRIVATE KEY-----
-`
+`)
+
+	BobsCert []byte = []byte(`-----BEGIN CERTIFICATE-----
+MIIEXDCCAsSgAwIBAgIQDjDy+g+JLRJJyOvQlUpnFDANBgkqhkiG9w0BAQsFADBZ
+MQswCQYDVQQGEwJVUzEqMCgGA1UEChMhU3RyZXhDb3JwIFN5bmVybmlzdHMgSW5j
+b3Jwb3JhdGVkMR4wHAYDVQQDExVLZXZpbnMtTUJQLnN0cmV4LmNvcnAwHhcNMTYx
+MjExMTkwMDA2WhcNMTcxMjExMTkwMDA2WjBZMQswCQYDVQQGEwJVUzEqMCgGA1UE
+ChMhU3RyZXhDb3JwIFN5bmVybmlzdHMgSW5jb3Jwb3JhdGVkMR4wHAYDVQQDExVL
+ZXZpbnMtTUJQLnN0cmV4LmNvcnAwggGiMA0GCSqGSIb3DQEBAQUAA4IBjwAwggGK
+AoIBgQC/H+si5BFSI944t3XZP2zGdfCpcm+uXkiC0W4tJjXk+/Mm0eb6TmuEcinO
+SPzqfqSEE9UxeQeRUEUA6/9Z1TRm75zhxCJpv0I1Lki1dk4YvjdO8Dmajbsn8LTr
+m7MlGdwNoEvlXpJiHxA2+N4ps/KpiYAPF6ZapEXBoHp+M3VzGZKCoEOavp6luI3r
+cmlHTqqZM1+dUt5Ozf3hVadg/8txhs+ivBuzM2j6E05DZgqzHW/UL155zp03Jx7b
+lYay1g0fDYmXwDG/9D5Kd8+gCAp103SOizfTBCPZJV9knDIsalEU8VUovX9YDUfo
+Oen2ORoBbrHXfNCwNXT/XFD1g6/DPUfX6ydw1JwOrNi6Vzo1o0DugdMd1er8EDYR
+S+MmYb5vGFu/QKoauFNkqZG92CKVGAK9Wl8ArF/UISbmv5oR7SjT+V2sclCYHrab
+2bTnRwlTs3q9+zhDMEthZFVDED70rTdr2hWh3qlBuGGbZVievLGUJJOHsmLim3ki
+DiZGbLUCAwEAAaMgMB4wDgYDVR0PAQH/BAQDAgWgMAwGA1UdEwEB/wQCMAAwDQYJ
+KoZIhvcNAQELBQADggGBAIHwlFtYX9y8xQspTg7B34FMXgqOgjsiEKJiL1noWIfk
+Pv1sZvoWZhmVTvzvVYDKsZYTVro+OOORms5Z6kk9MV5N4hWuTG/Zij1Q+UcQTqmz
+J/LigQtVfeOlcYrRlRh/v0cTaHyXjeRM+c1MeNdfnWgTZnEcZwGqvHJrE9nPlTsb
+/ss3saLh+8S/yS0qtLgKPCrUQHzJNt1Sp8irceH4388dlIy7ebtooyJk0DFBhWeY
+H4WdRh7vs39wrNZW5Um/yeH8M+djmPDynhcbhKQU0jU6DgQtmtOGrJfmN7HJFHwi
+3Lf4qaIC7WCHE0lLELdT+dXWTO8vOG8pS1VpDAx0lX8Yaq8uYjalJm/FHH2VYpH8
+FQl/dQe68Z0vp8L7at9v8suvjaNaPB5eW+PDUfoEmQ3MGwbRo2BHa8wGC0/zhlNl
+FOse5ft23HwKvSJSfK5LSGo51KoHnaR0/1Vr5uLqvJAVAGdNHYlo4+YVbUkVbZ/j
+0/cdnqxVgDhmT6dMZlqzWQ==
+-----END CERTIFICATE-----
+`)
+
+	BobsKey []byte = []byte(`-----BEGIN RSA PRIVATE KEY-----
+MIIG4wIBAAKCAYEAvx/rIuQRUiPeOLd12T9sxnXwqXJvrl5IgtFuLSY15PvzJtHm
++k5rhHIpzkj86n6khBPVMXkHkVBFAOv/WdU0Zu+c4cQiab9CNS5ItXZOGL43TvA5
+mo27J/C065uzJRncDaBL5V6SYh8QNvjeKbPyqYmADxemWqRFwaB6fjN1cxmSgqBD
+mr6epbiN63JpR06qmTNfnVLeTs394VWnYP/LcYbPorwbszNo+hNOQ2YKsx1v1C9e
+ec6dNyce25WGstYNHw2Jl8Axv/Q+SnfPoAgKddN0jos30wQj2SVfZJwyLGpRFPFV
+KL1/WA1H6Dnp9jkaAW6x13zQsDV0/1xQ9YOvwz1H1+sncNScDqzYulc6NaNA7oHT
+HdXq/BA2EUvjJmG+bxhbv0CqGrhTZKmRvdgilRgCvVpfAKxf1CEm5r+aEe0o0/ld
+rHJQmB62m9m050cJU7N6vfs4QzBLYWRVQxA+9K03a9oVod6pQbhhm2VYnryxlCST
+h7Ji4pt5Ig4mRmy1AgMBAAECggGAeIk0zkhv9BHS8IojtOOfQnrXUMZvUNT9fiN/
+DNJwYv3TF8SBm5Mhk+I8I3E8sNc2AmZPqmfeMfMh0bcE0C76YYD0vB3qZi7NHqUW
+DfuSvWAZmZs/eQ2x3zqbn4wOq3NnqOBTfVDrmR5mJ4VE96AZhnrFXL9ZlwjLRZZk
+KnthszmJgQB6InndrD58ouUBKIugZAZWYu4EbkbTuu0+JpqsP+j5wnRIM/hUWIMc
+dL1COjxybquBNejslgACIhJnZRpBDSK832HPlH8JGC9aR6/CJE6sJeR433UeD0UL
+BSxMWp9KVaYHsdfbNpBNcnOMSz4C7Fi1xz/En/zdfLkolO1OtwxdkoREf4OXDb0x
+G0YutigWAXG3RCZXSD66UT/DxL6EAiDaRZ9N0D10j9C5qEjb+wGXxkgffNzHnt4W
+tbZHhPkQLuCRElWTS+CvZxykKvYlKqsoxLJMq9qlisU7kXykuJ2JrXRfrwNaDoVc
+c1JDJoDOnLTGczhsTmUo1eg9hfPxAoHBAOJ/0Dw1/Og9UupJoQIkjoVFe2jZMQx9
+Yu/1zTo+Gdj8M6hhTsxANw5gMNhdxLB6kWwS8ZTxRFfmswLp3vfrDTH1/fEmnlsM
+Ngn55jMTGoZTtfvr3iXp9BonKm1TuMDSRoi7CqTkSdJKySPoRRwBiq7yTkfy1RPk
+/qY+m0OeVv31HhZoZHowKJtQ8GxQuPFlk/RHXMYXNdt9fgnFJdu4E3qqV4cOTGYh
+NJejIXnr50ynlrr0JQeXsYw5bIpAkXsiGwKBwQDYBJtXiNNQYJfwOC0Jf6EtEyko
+XgWpOuIVCwnW+qncY3StAAY2os9Yl/tSWZB7OB2j1EsAChmDre8kHsWkkWcnpMLJ
+GcR/Mymu2iYIX3zi3/rfHUYFSk+in1+gvFo77J4fqespz4ik6t8sey1PciEN4Gq0
+SLUtCyLfUBc4hFq3zWWj62cH2h2sD+2HlcwfVvcp9LSAh9AsPiDGkmMElwj7ZaNm
+Qef7oNvXM646AnU0oA+XV08Pm6ZFNHF1L4uKGW8CgcAbq01+7YF1xXgJkvEOV/Tp
+JvHKfy3UvsSFV9u4T7nMnhKZcTm1Desr4GhXJi1O+comD7JfZZHZwx7/Lh7E3nwA
+LHLXMMaLjNRVJ6gNeTn2SueiXNAIhaoCP+KY9G9PbuCa9253ckPqYqRrtIYsw6tz
+b7pMY8fP5FuPZ3qEKiWqZHVGe346xwNO5fZY5QuSwbvvTYBx2ogDxjLNcr4LN1N5
+2rFSmFaQ62GHXLBMOptI6/gn4odbhBZ2cNVnljeveR0CgcEAkBEieRKru+Nv8anF
+f5w7JhCtVlq+c8rtlRMzkMjEGFPMMMTfX/jdSDy0RIuvLD6UpfqjuncB5SxPXUuY
+jNTcczgTIIOq5Rp+JjsfOl5UsAqpCbxAQq74xYB9CnoTw/teycdNKylZ/IMYLFZK
+Nb3sBZEyGOKU0mGm9EA5/FtpFURLETOiFz6Eo4hL5i0lYZFibfzhlQb+80LMISLo
+HtuMBf4XJ8+0o8D4XWH+RHn3KQ3G2CxGciZgka8ULA91hLjJAoHAWEnaSOzyY/0G
+D/YVxNKQiQvYY7nH0KnRWE38RZ7O6vJkQYieRAKW50Mb103WMEm8i/J6pdPKxKoP
+U+GUk8SK/t4vlvxCTUQa5KRWmLlPJLNXeXMioXZYkarzt1sgmtdQ3NLlnGR5/0sg
+1Id5qZFPi+2a5TXRI2hLn/22o2aJmXIYrnz06HnmVizIPiEGkYPP8Xf/jFeMI7Xp
+8J2Dor46N5fpqJSknfD1d8HicBUzyQsc06LWqRMEQ3NzXrSE2/iy
+-----END RSA PRIVATE KEY-----
+`)
+
 	SecretData []byte = []byte(`Congress shall make no law respecting an establishment of
 religion, or prohibiting the free exercise thereof; or abridging the freedom of
 speech, or of the press; or the right of the people peaceably to assemble, and
